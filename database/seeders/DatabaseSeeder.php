f67b4419826db48fab0fecc9dc7ee623ace5865e
Ayoub CHAHIR <ayoub.chahir@newcogroup.com>
2023-09-19 13:04:39 +0100

    Blog starting point

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        Post::factory(20)->create();
+
+        User::find(1)->update([
+            'name' => 'Andre',
+            'email' => 'andre@andre.com'
+        ]);
+    }
+}
